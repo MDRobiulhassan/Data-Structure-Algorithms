e4732ecc8e8edc16d54c040758021411360809aa
MD Robiul Hassan <71929943+MDRobiulhassan@users.noreply.github.com>
2026-02-12 20:21:18 +0600

    String Hard

---
+++ String/Hard/palindromic-substrings.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int countSubstrings(string s)
+    {
+        int n = s.size();
+        int count = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            int left = i, right = i;
+
+            while (left >= 0 && right < n && s[left] == s[right])
+            {
+                left--;
+                right++;
+                count++;
+            }
+
+            left = i, right = i + 1;
+            while (left >= 0 && right < n && s[left] == s[right])
+            {
+                left--;
+                right++;
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
