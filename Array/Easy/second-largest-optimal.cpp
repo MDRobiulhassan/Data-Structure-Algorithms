5ef9f840d6d9fbb5403d8dc54b02dc8e36e5322e
MDRobiulHassan <robiul110256@gmail.com>
2026-08-31 19:37:42 +0600

    GFG - largest and second largest element in an array

--- Array/Easy/second-largest-optimal.cpp
+++ Array/Easy/second-largest-optimal.cpp
@@ -1,48 +1,27 @@
-//{ Driver Code Starts
 #include <bits/stdc++.h>
-
 using namespace std;
 
-// } Driver Code Ends
-//User function template for C++
-class Solution{
-public:	
-	// Function returns the second
-	// largest elements
-	int print2largest(int a[], int n) {
-	    int l=a[0],sl=-1;
-	    for(int i=0;i<n;i++)
-	    {
-	        if(a[i]>l)
-	        {
-	            sl=l;
-	            l=a[i];
-	        }
-	        else if(a[i]>sl&&a[i]<l)
-	            sl=a[i];
-	    }
-	    return sl;
+class Solution
+{
+public:
+	int getSecondLargest(vector<int> &arr)
+	{
+		int mx = -1;
+		int secMax = -1;
+
+		for (int x : arr)
+		{
+			if (x > mx)
+			{
+				secMax = mx;
+				mx = x;
+			}
+			else if (x < mx && x > secMax)
+			{
+				secMax = x;
+			}
+		}
+
+		return secMax;
 	}
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-        int arr[n];
-        for (int i = 0; i < n; i++) {
-            cin >> arr[i];
-        }
-        Solution ob;
-        auto ans = ob.print2largest(arr, n);
-        cout << ans << "\n";
-    }
-    return 0;
-}
-
-// } Driver Code Ends
