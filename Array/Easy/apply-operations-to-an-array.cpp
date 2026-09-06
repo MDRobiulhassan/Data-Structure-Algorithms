51d64d4fcd8159052ce30f6c1e3b1616926ab5f2
MDRobiulHassan <robiul110256@gmail.com>
2026-09-06 19:45:12 +0600

    LeetCode 2460. Apply Operations to an Array and 283. Move Zeroes

---
+++ Array/Easy/apply-operations-to-an-array.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> applyOperations(vector<int> &nums)
+    {
+        int n = nums.size();
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            if (nums[i] == nums[i + 1])
+            {
+                nums[i] *= 2;
+                nums[i + 1] = 0;
+            }
+        }
+
+        int j = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] != 0)
+                swap(nums[i], nums[j++]);
+        }
+
+        return nums;
+    }
+};
