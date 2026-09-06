e055a27fac020347d6c175910e14010187718ba7
MDRobiulHassan <robiul110256@gmail.com>
2026-09-06 19:24:00 +0600

    LeetCode 189. Rotate Array

---
+++ Array/Easy/rotate-array-brute.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+	void rotate(vector<int> &nums, int k)
+	{
+		int n = nums.size();
+
+		k = k % n;
+
+		vector<int> numsCopy = nums;
+
+		for (int i = 0; i < n; i++)
+		{
+			numsCopy[(i + k) % n] = nums[i];
+		}
+
+		nums = numsCopy;
+	}
+};
