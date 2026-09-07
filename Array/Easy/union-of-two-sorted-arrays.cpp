d739cd6ccb3a4425898b40ca41bbf2387eec3578
MDRobiulHassan <robiul110256@gmail.com>
2026-09-07 19:09:35 +0600

    Union of Two Sorted Array - GFG

---
+++ Array/Easy/union-of-two-sorted-arrays.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> findUnion(vector<int> &a, vector<int> &b)
+    {
+        int n = a.size(), m = b.size();
+        vector<int> ans;
+        int i = 0, j = 0;
+
+        while (i < n && j < m)
+        {
+            if (a[i] < b[j])
+            {
+                if (ans.empty() || ans.back() != a[i])
+                    ans.push_back(a[i]);
+                i++;
+            }
+
+            else if (b[j] < a[i])
+            {
+                if (ans.empty() || ans.back() != b[j])
+                    ans.push_back(b[j]);
+                j++;
+            }
+
+            else
+            {
+                if (ans.empty() || ans.back() != a[i])
+                    ans.push_back(a[i]);
+                i++;
+                j++;
+            }
+        }
+
+        while (i < n)
+        {
+            if (ans.empty() || ans.back() != a[i])
+                ans.push_back(a[i]);
+            i++;
+        }
+
+        while (j < m)
+        {
+            if (ans.empty() || ans.back() != b[j])
+                ans.push_back(b[j]);
+            j++;
+        }
+
+        return ans;
+    }
+};
