eaf2b0b5a4b16e51415e5460296007250d0f5d0d
MDRobiulHassan <robiul110256@gmail.com>
2026-09-19 18:57:16 +0600

    Longest Subarray with Sum K

--- Array/Problems/longestsubarray0sum.cpp
+++ Array/Problems/longestsubarray0sum.cpp
@@ -1,12 +1,5 @@
-//{ Driver Code Starts
-// Initial Template for C++
-
 #include <bits/stdc++.h>
 using namespace std;
-
-// } Driver Code Ends
-/*You are required to complete this function*/
-
 class Solution
 {
 public:
@@ -30,26 +23,3 @@ public:
         return maxlen;
     }
 };
-
-//{ Driver Code Starts.
-
-int main()
-{
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        int m;
-        cin >> m;
-        vector<int> array1(m);
-        for (int i = 0; i < m; ++i)
-        {
-            cin >> array1[i];
-        }
-        Solution ob;
-        cout << ob.maxLen(array1, m) << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
